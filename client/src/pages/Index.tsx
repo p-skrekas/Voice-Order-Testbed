12eb76ac82f74be5bd41b21f0668e2e84ec3c51f
p-skrekas <paschalis.skrekas@reborrn.com>
2024-11-28 10:18:51 +0200

    update home layout

--- client/src/pages/Index.tsx
+++ client/src/pages/Index.tsx
@@ -1,13 +1,12 @@
-import { Link } from "react-router-dom";
 import { Button } from "../components/ui/button";
 import { Card, CardHeader, CardTitle, CardContent, CardDescription } from "../components/ui/card";
 import { useNavigate } from "react-router-dom";
-import { MicVocal } from 'lucide-react';
+import { MicVocal, BotMessageSquare, Wrench, Videotape  } from 'lucide-react';
 
 export default function Index() {
     const navigate = useNavigate();
     return (
-        <div className="flex w-full h-full p-3">
+        <div className="flex flex-col items-center justify-start md:flex-row md:items-start md:justify-start md:flex-wrap w-full p-3 gap-3">
 
             <Card className="flex flex-col w-[300px] h-[300px] justify-between">
                 <CardHeader className="flex flex-col items-center">
@@ -28,6 +27,76 @@ export default function Index() {
                         navigate('/synthetic-voice-orders');
                     }}>
                         Generate synthetic voice orders
+                    </Button>
+                </CardContent>
+            </Card>
+
+            
+            <Card className="flex flex-col w-[300px] h-[300px] justify-between">
+                <CardHeader className="flex flex-col items-center">
+                    <CardTitle>
+                        <div className="flex flex-col items-center gap-6 mb-3">
+                            <Videotape size={32} />
+                            View recorded voices
+                        </div>
+                    </CardTitle>
+                    <CardDescription>
+                        <span>
+                            View and manage recorded voices.
+                        </span>
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="items-center">
+                    <Button className="w-full" onClick={() => {
+                        navigate('/recorded-voices');
+                    }}>
+                        View recorded voices
+                    </Button>
+                </CardContent>
+            </Card>
+            
+            <Card className="flex flex-col w-[300px] h-[300px] justify-between">
+                <CardHeader className="flex flex-col items-center">
+                    <CardTitle>
+                        <div className="flex flex-col items-center gap-6 mb-3">
+                            <BotMessageSquare  size={32} />
+                            Chat with AI
+                        </div>
+                    </CardTitle>
+                    <CardDescription>
+                        <span>
+                            Test Sonent and Haiku models to test their capabilities.
+                        </span>
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="items-center">
+                    <Button className="w-full" onClick={() => {
+                        navigate('/chat');
+                    }}>
+                        Chat with AI
+                    </Button>
+                </CardContent>
+            </Card>
+
+            <Card className="flex flex-col w-[300px] h-[300px] justify-between">
+                <CardHeader className="flex flex-col items-center">
+                    <CardTitle>
+                        <div className="flex flex-col items-center gap-6 mb-3">
+                            <Wrench  size={32} />
+                            Prompt Optimizer
+                        </div>
+                    </CardTitle>
+                    <CardDescription>
+                        <span>
+                            Modify the prompts for Sonnet and Haiku models.
+                        </span>
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="items-center">
+                    <Button className="w-full" onClick={() => {
+                        navigate('/settings');
+                    }}>
+                        Modify prompts
                     </Button>
                 </CardContent>
             </Card>
